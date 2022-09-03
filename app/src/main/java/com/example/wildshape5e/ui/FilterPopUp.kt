372f8fcbbde4c3b56ae03e2132fded45e487e75f
Nikhil Jahagirdar <nxj200009@utdallas.edu>
2022-09-02 23:36:53 -0500

    Filter can now be clicked off of by hitting background.

--- app/src/main/java/com/example/wildshape5e/ui/FilterPopUp.kt
+++ app/src/main/java/com/example/wildshape5e/ui/FilterPopUp.kt
@@ -1,31 +1,34 @@
 package com.example.wildshape5e.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 @Composable
-fun FilterPopUp(name : String, onDone: (() -> Unit) = {}, onReset: (() -> Unit) = {}, content: @Composable (() -> Unit)) {
+fun FilterPopUp(
+    name: String,
+    onDone: (() -> Unit) = {},
+    onReset: (() -> Unit) = {},
+    content: @Composable (() -> Unit)
+) {
     Column(
         modifier = Modifier
             .fillMaxSize(),
         verticalArrangement = Arrangement.Bottom
     ) {
-        Spacer(
+        Button(
             modifier = Modifier
                 .weight(1f)
-                .background(Brush.verticalGradient(colors = listOf(Color.Transparent, Color.Black)))
-                .clickable { onDone() }
-        )
+                .fillMaxWidth(),
+            onClick = { onDone() },
+            colors = ButtonDefaults.buttonColors(Color.Transparent)
+        ) {}
         Column(
             modifier = Modifier
                 .fillMaxWidth()
